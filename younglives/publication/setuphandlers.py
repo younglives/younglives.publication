6a769c4d7c7e8712b6214089136e49384bdcdede
Michael Davis <m.r.davis@me.com>
2015-03-28 14:31:31 +0000

    Finish publication migration script

--- younglives/publication/setuphandlers.py
+++ younglives/publication/setuphandlers.py
@@ -7,11 +7,21 @@ from plone.app.linkintegrity.info import LinkIntegrityInfo
 from Products.Archetypes.event import ObjectEditedEvent
 from Products.Archetypes.event import ObjectInitializedEvent
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.WorkflowCore import WorkflowException
 import transaction
 from zope.app.component.hooks import getSite
 from zope.lifecycleevent import modified
 from zope.publisher.interfaces import NotFound
 import zope.event
+
+referencedRelationship = 'isReferencing'
+
+images_to_keep = [
+    'changing-lives-in-a-changing-world-pub',
+    'NothingisImpossible_coverscan.jpg',
+    'pp6-murray',
+]
+
 
 class my_parser(HTMLParser):
 
@@ -62,18 +72,22 @@ def importVarious(context):
     """
     portal = getSite()
     catalog = getToolByName(portal, 'portal_catalog')
+    refcat = getToolByName(portal, 'reference_catalog')
+    wftool = getToolByName(portal, 'portal_workflow')
     items = catalog.searchResults({'meta_type': 'Publication', })
     parser = my_parser()
     tag_dropper = TagDropper(['img',])
     failed_covers = []
+    broken_references = []
     for item in items:
         transaction.begin()
         object = item.getObject()
         object_id = object.getId()
+        if object_id != 'nutritional-trajectories-in-childhood-Peru-Spanish':
+            pass
         body_text = object.getText()
         parent_folder = object.aq_inner.aq_parent
         parent_folder.manage_renameObject(object_id, object_id + "-old")
-        transaction.commit()
         parent_folder.invokeFactory(
             'YLPublication',
             object_id,
@@ -87,34 +101,30 @@ def importVarious(context):
         pub_files = object.getFiles()
         for pub_file in pub_files:
             move_object(ob=pub_file, dest=new_object)
+        # delete the file references
+        object.setFiles([])
+        parser.images = []
         parser.feed(body_text)
         if len(parser.images) == 1:
             # only one image, so assume it's the cover
-            if len(parser.images[0]) > 28:
+            #if len(parser.images[0]) > 28:
+            if parser.images[0].find('publication-cover-images') > -1:
                 path = parser.images[0].split('/')
-                print path
                 if path[3] in ['portal', 'ethiopia', 'india', 'vietnam',]:
                     path = '/'.join(path[4:])
                 else:
                     path = '/'.join(path[3:])
-                print path
                 path = path.split('@@images')
-                if len(path) > 1:
-                    import pdb;pdb.set_trace()
-                print path
                 path = path[0]
-                print path
                 try:
                     the_image = portal.unrestrictedTraverse(path)
-                except NotFound:
-                    import pdb;pdb.set_trace()
+                except (NotFound, AttributeError):
+                    pass
                 else:
                     new_object.setCover_image(the_image.data)
-                # remove the image anyway, as it's broken
                 tag_dropper.feed(body_text)
                 new_body_text = tag_dropper.get_text()
                 new_object.setText(new_body_text)
-                new_object.reindexObject()
                 zope.event.notify(ObjectEditedEvent(new_object))
                 modified(new_object)
                 # remove the reference to the image
@@ -124,33 +134,32 @@ def importVarious(context):
                 modified(object)
                 # delete the image
                 image_folder = the_image.aq_inner.aq_parent
-                transaction.commit()
-                transaction.begin()
-                try:
-                    image_folder.manage_delObjects([the_image.getId(),])
-                    print 'Image deleted: ' + the_image
-                except LinkIntegrityNotificationException:
-                    # still linked by something else
-                    import pdb;pdb.set_trace()
-                    pass
+                if the_image.getId() not in images_to_keep:
+                    try:
+                        image_folder.manage_delObjects([the_image.getId(),])
+                    except LinkIntegrityNotificationException:
+                        pass
             else:
                 failed_covers.append(object.getId())
+                new_object.setText(body_text)
         else:
             failed_covers.append(object.getId())
-        if object.getRelatedItems():
-            print 'foobar'
-            import pdb;pdb.set_trace()
-        if object.getReferences():
-            print 'foobar2'
-            import pdb;pdb.set_trace()
-        if object.getBackReferences():
-            print 'foobar3'
-            import pdb;pdb.set_trace()
+            new_object.setText(body_text)
+        new_object.reindexObject()
         try:
-            import pdb;pdb.set_trace()
             parent_folder.manage_delObjects([object.getId(),])
         except LinkIntegrityNotificationException:
-            print 'barfoo'
-            import pdb;pdb.set_trace()
+            broken_references.append(object.id)
+        publish_new = True
+        try:
+            wftool.doActionFor(object, 'retract')
+        except WorkflowException:
+            # item not published
+            publish_new = False
+        if publish_new:
+            try:
+                wftool.doActionFor(new_object, 'publish')
+            except WorkflowException:
+                pass
         transaction.commit()
-    return failed_covers
+    return failed_covers, broken_references
